3e568f383a8153b595fe6f5a26e4a42c7945064c
bilibili <623059008@qq.com>
2017-12-11 19:58:29 +0800

    Add gevent packages

---
+++ transactions.js
@@ -0,0 +1,8 @@
+transactions:[
+'sender':sender,
+'recipient':recip,
+'amount':num,
+'timestamp':time,
+'public_key':(n,e)
+//The public key of recipient who must use his //private key to confirm this transaction.
+]
